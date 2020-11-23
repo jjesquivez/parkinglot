7e04942573b1757d7d3803b02aba48b7f24bbeeb
Ricardo Sanchez Santos <ricardo.sanchez@definityfirst.com>
2020-11-23 16:43:35 -0600

    Add project files.

---
+++ parkinglot/Database/Model/ClientsModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parkinglot.Database.Model
+{
+    [Table("clients")]
+    public class ClientsModel
+    {
+        [Key]
+        [Column("id")]
+        public string id { get; set; }
+        [Column("brand")]
+        public string brand { get; set; }
+        [Column("model")]
+        public string model { get; set; }
+        [Column("plates")]
+        public string plates { get; set; }
+        [Column("enter")]
+        public DateTime enter { get; set; }
+        [Column("exit")]
+        public DateTime exit { get; set; }
+
+    }
+}
---
+++ parkinglot/Database/ParkinglotContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using parkinglot.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parkinglot.Database
+{
+    public class ParkinglotContext : DbContext, IParkingContext
+    {
+
+        #region Constructor
+        public ParkinglotContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        #endregion
+    }
+}
